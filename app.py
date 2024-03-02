d7207c8daac8cfca34a831189553ffd86b355bb9
kevinb0071 <69095534+kevinb0071@users.noreply.github.com>
2024-03-02 22:28:33 +0000

    Added the Flask project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return "Hello kevin!"
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
