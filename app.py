de3f4dbb28244288a9478e0dcebda7a06d9096f8
kevinb0071 <69095534+kevinb0071@users.noreply.github.com>
2024-03-02 23:13:09 +0000

    Added HTML Template

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-from flask import Flask, render_template, request
-
-
-
-
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
-@app.route('/index')
-def index():
-  return "Hello kevin!"
-
-
+@app.route('/home')
+def home():
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
