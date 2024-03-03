cce19bce3f4e30d9157d3c1104b1056fc514680f
kevinb0071 <69095534+kevinb0071@users.noreply.github.com>
2024-03-03 20:32:46 +0000

    Create api

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
 
+Jobs = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': '$100,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location':'Delhi, India',
+    'salary': '$150,000'
+  },
+    {
+      'id': 3,
+      'title': 'Frontend Engineer',
+      'location': 'Remote',
+      'salary': '$120,000'
+    },
+  {
+     'id': 4,
+      'title': 'Backend Engineer',
+      'location': 'San Francisco, USA',
+      'salary': '$150,000'
+  }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', jobs=Jobs)
 
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(Jobs)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
