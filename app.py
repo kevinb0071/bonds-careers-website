017422a74dd40d21c23370642ff6cab0789c9aff
Kevin Bonds <kevin.bonds@gmail.com>
2024-03-04 16:40:35 -0600

    created job page

--- app.py
+++ app.py
@@ -11,76 +11,82 @@ app.secret_key = b"\xa2I\x14jZ\x80\x12\xc7"
 
 
 class Base(DeclarativeBase):
-  pass
+    pass
 
 
 db = SQLAlchemy(app, model_class=Base)
 
 
 class Jobs(db.Model):
-  id: Mapped[int] = mapped_column(db.Integer(), primary_key=True)
-  title: Mapped[str] = mapped_column(db.String(100))
-  location: Mapped[str] = mapped_column(db.String(255))
-  salary: Mapped[int] = mapped_column(db.Integer())
-  currency: Mapped[str] = mapped_column(db.String(10))
-  requirements: Mapped[str] = mapped_column(db.String(255))
-  responsibilities: Mapped[str] = mapped_column(db.String(255))
-  created_at: Mapped[datetime] = mapped_column(
-      default=lambda: datetime.now(timezone.utc))
-  updated_at: Mapped[datetime] = mapped_column(
-      default=lambda: datetime.now(timezone.utc),
-      onupdate=lambda: datetime.now(timezone.utc))
-  @property
-  def prettier_salary(self):
+    id: Mapped[int] = mapped_column(db.Integer(), primary_key=True)
+    title: Mapped[str] = mapped_column(db.String(100))
+    location: Mapped[str] = mapped_column(db.String(255))
+    salary: Mapped[int] = mapped_column(db.Integer())
+    currency: Mapped[str] = mapped_column(db.String(10))
+    requirements: Mapped[str] = mapped_column(db.String(255))
+    responsibilities: Mapped[str] = mapped_column(db.String(255))
+    created_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
+
+    @property
+    def prettier_salary(self):
         if len(str(self.salary)) >= 4:
-            return f'${str(self.salary)[:-3]},{str(self.salary)[-3:]}'
+            return f"${str(self.salary)[:-3]},{str(self.salary)[-3:]}"
         else:
             return f"${self.salary}"
-  def __repr__(self):
-    return f"{self.title}"
+
+    def __repr__(self):
+        return f"{self.title}"
 
 
 with app.app_context():
-  db.create_all()
+    db.create_all()
 
 
 class JobForm(FlaskForm):
-  title = StringField(label="Title:")
-  location = StringField(label="Location:")
-  salary = IntegerField(label="Salary:")
-  currency = StringField(label="Currency:")
-  requirements = StringField(label="Requirements:")
-  responsibilities = TextAreaField(label="Responsibilities:")
-  submit = SubmitField("Submit")
+    title = StringField(label="Title:")
+    location = StringField(label="Location:")
+    salary = IntegerField(label="Salary:")
+    currency = StringField(label="Currency:")
+    requirements = StringField(label="Requirements:")
+    responsibilities = TextAreaField(label="Responsibilities:")
+    submit = SubmitField("Submit")
 
 
-
-
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-  jobs = Jobs.query.all()
-  return render_template('home.html', jobs=jobs)
+    jobs = Jobs.query.all()
+    return render_template("home.html", jobs=jobs)
 
-@app.route('/add', methods=['GET', 'POST'])
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-  form = JobForm()
-  if form.validate_on_submit():
-    job = Jobs(title=form.title.data, 
-               location=form.location.data, 
-               salary=form.salary.data, 
-               currency=form.currency.data, 
-               requirements=form.requirements.data, 
-               responsibilities=form.responsibilities.data)
-    db.session.add(job)
-    db.session.commit()
-    return redirect(url_for('home'))
-  return render_template('addjob.html', form=form)
+    form = JobForm()
+    if form.validate_on_submit():
+        job = Jobs(
+            title=form.title.data,
+            location=form.location.data,
+            salary=form.salary.data,
+            currency=form.currency.data,
+            requirements=form.requirements.data,
+            responsibilities=form.responsibilities.data,
+        )
+        db.session.add(job)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("addjob.html", form=form)
 
+@app.route('/job/<int:id>')
+def show_job(id):
+    job = Jobs.query.filter_by(id=id).first()
+    return render_template('job.html', job=job)
+    
 
-@app.route('/api/jobs')
-def list_jobs():
-  return jsonify(Jobs)
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
